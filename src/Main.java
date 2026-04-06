412c2ee831276e903268974f33e2e1cd46a15051
NOHKYEONGMIN <sji4844@naver.com>
2026-04-06 12:29:06 +0900

    feat: Add welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, LikeLion!");
+        System.out.println("Welcome to Backend Course!");
     }
 }
