06671c4407317aaff08ffd0d21eb8518790c071d
NOHKYEONGMIN <sji4844@naver.com>
2026-04-06 12:45:31 +0900

    멋쟁이사자 미션 1: 아기사자 명단 관리 프로그램

---
+++ src/Mission01.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Mission01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        while (count < 5) {
+            System.out.print("아기사자 인원 수를 입력하세요 (5명 이상): ");
+            count = scanner.nextInt();
+            if (count < 5) {
+                System.out.println("5명 이상 입력해주세요!");
+            }
+        }
+
+        String[] names = new String[count];
+        scanner.nextLine(); // 버퍼 비우기
+
+        for (int i = 0; i < count; i++) {
+            System.out.print((i + 1) + "번째 아기사자 이름: ");
+            names[i] = scanner.nextLine();
+        }
+
+        System.out.println("\n===== 아기사자 명단 =====");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println((i + 1) + ". " + names[i]);
+        }
+        System.out.println("총 " + count + "명");
+
+        scanner.close();
+    }
+}
