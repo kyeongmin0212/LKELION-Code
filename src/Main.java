5a2d49e3b17b1dd0201e6f4841caef8e8f17ed1f
NOHKYEONGMIN <sji4844@naver.com>
2026-04-06 12:29:24 +0900

    feat: Add motivation message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, LikeLion!");
         System.out.println("Welcome to Backend Course!");
+        System.out.println("Let's start coding with Java!");
     }
 }
